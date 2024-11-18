d608d8ad83343bd1555f4bce377cb3c305f02026
Sophie Muchiri <muthonisophie12@gmail.com>
2024-11-18 20:58:15 +0300

    FLASKrestful CRUD

--- server/app.py
+++ server/app.py
@@ -16,7 +16,6 @@ db.init_app(app)
 
 api = Api(app)
 
-
 class Plants(Resource):
 
     def get(self):
@@ -37,19 +36,50 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
-
 
 class PlantByID(Resource):
 
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
 
+        data = request.get_json()
+
+        for key, value in data.items():
+            setattr(plant, key, value)
+
+        db.session.add(plant)
+        db.session.commit()
+
+        response_dict = plant.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+
+        return response
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        # response_dict = {"message": "Plant deleted\n"}
+        #You can uncomment the response_dict and replace the empty response with response_dict
+        response = make_response(
+            '',
+            200
+        )
+
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
